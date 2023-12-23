50b7c96040060cb1149be18ff126ccb7421bed6c
Aliweb23 <haschimi7219@gmail.com>
2023-12-23 18:51:23 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    // Menü-Click-Ereignis
+    $('#menu').click(function() {
+        $(this).toggleClass('fa-times'); // Toggle-Klasse für das Menü-Symbol
+        $('header').toggleClass('toggle'); // Toggle-Klasse für den Header
+    });
+
+    // Scroll- und Ladeereignis
+    $(window).on('scroll load', function() {
+        // Bei Scrollen oder Laden, Klassen entfernen
+        $('#menu').removeClass('fa-times'); // Klasse für das Menü-Symbol entfernen
+        $('header').removeClass('toggle'); // Klasse für den Header entfernen
+        if ($(window).scrollTop() > 0) {
+            $('.top').show();
+        } else {
+            $('.top').hide();
+        }
+    });
+
+    // smooth scrolling
+    $('a[href*="#"]').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top,
+        }, 500, 'linear');
+    });
+});
